e3589811d2a98b5719dd97528c94d7dc20bbf9af
Felipe <fsa398975@gmail.com>
2026-04-04 15:24:08 -0300

    Update q01.java

--- semester-2/algorithms-and-data-structures-2/tp-1/q01.java
+++ semester-2/algorithms-and-data-structures-2/tp-1/q01.java
@@ -2,39 +2,23 @@ import java.util.Scanner;
 
 public class q1 {
 
-    // aplica um deslocamento de +3 em cada caractere da string
+    // Applies a Caesar cipher with shift +3
     public static void criptografar(String string){
 
-        // array para armazenar os novos caracteres
         char[] array = new char[string.length()];
 
-        // percorre a string caractere por caractere
         for(int i = 0; i < string.length(); i++){
-
-            // pega o caractere atual e soma 3 (cifra simples)
-            char letra = (char)(string.charAt(i) + 3);
-
-            // salva o novo caractere no array
-            array[i] = letra;
+            array[i] = (char)(string.charAt(i) + 3);
         }
 
-        // transforma o array em string novamente
-        String stringfinal = new String(array);
-
-        // imprime o resultado
-        System.out.println(stringfinal);
+        System.out.println(new String(array));
     }
 
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-
-        // lê a linha digitada pelo usuário
         String string = sc.nextLine();
 
-        // chama a função de criptografia
         criptografar(string);
-
-        sc.close();
     }
 }
