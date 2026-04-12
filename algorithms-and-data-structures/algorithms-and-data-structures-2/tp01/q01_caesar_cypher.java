02e0f13990808f7eae5d4c79ba4e9450a61d1231
Felipe <fsa398975@gmail.com>
2026-04-11 21:08:38 -0300

    Update q01_caesar_cypher.java

--- algorithms-and-data-structures/algorithms-and-data-structures-2/tp01/q01_caesar_cypher.java
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/tp01/q01_caesar_cypher.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class q1 {
 
-    // Shifts each character in the string by +3 (Caesar cipher)
+    // Shifts each character in the string by +3 (Caesar cipher style)
     public static void criptografar(String string){
         char[] array = new char[string.length()];
 
         for(int i = 0; i < string.length(); i++){
-            array[i] = (char)(string.charAt(i) + 3);
+            array[i] = (char)(string.charAt(i) + 3); // shift character
         }
 
-        String result = new String(array);
-        System.out.println(result);
+        System.out.println(new String(array)); // print result
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.nextLine();
-        criptografar(input);
+        // Read multiple lines until EOF
+        while(sc.hasNextLine()){
+            String input = sc.nextLine();
+            criptografar(input); // process each line
+        }
+
+        sc.close();
     }
 }
