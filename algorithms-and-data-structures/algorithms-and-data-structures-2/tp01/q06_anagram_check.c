7fd8da121242490bd3f7b712a6ce530c79d4effc
Felipe <fsa398975@gmail.com>
2026-04-12 15:25:16 -0300

    Implement case-insensitive anagram check
    Added a toLower function to convert strings to lowercase before checking for anagrams. Updated the anagrama function to return 1 for anagrams and improved the logic for checking character matches.

--- algorithms-and-data-structures/algorithms-and-data-structures-2/tp01/q06_anagram_check.c
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/tp01/q06_anagram_check.c
@@ -18,45 +18,58 @@ void removerEnter(char string[]){
     }
 }
 
-// Checks if two strings are anagrams (consumes str2 during process)
+// Converts string to lowercase
+void toLower(char str[]){
+    for(int i = 0; str[i] != '\0'; i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] = str[i] + 32;
+        }
+    }
+}
+
+// Checks if two strings are anagrams
 int anagrama(char str1[], char str2[]){
     int k1 = tam(str1), k2 = tam(str2);
 
-    if(k1 == k2){
+    if(k1 != k2){
+        return 0;
+    }
 
-        for(int i = 0; str1[i] != '\0'; i++){
-            int achou = 0;
+    for(int i = 0; str1[i] != '\0'; i++){
+        int achou = 0;
 
-            for(int k = 0; str2[k] != '\0'; k++){
-                if(str1[i] == str2[k] && achou == 0){
-                    achou++;
-                    str2[k] = 1; // mark as used
-                }
-            }
-
-            if(achou == 0){
-                return 0;
+        for(int k = 0; str2[k] != '\0'; k++){
+            if(str1[i] == str2[k] && achou == 0){
+                achou = 1;
+                str2[k] = 1;
             }
         }
 
-        return 1;
+        if(achou == 0){
+            return 0;
+        }
     }
 
-    return 0;
+    return 1;
 }
 
 int main(){
     char str1[255], str2[255];
 
     fgets(str1, 255, stdin);
-    removerEnter(str1);
-
     fgets(str2, 255, stdin);
+
+    removerEnter(str1);
     removerEnter(str2);
 
+    toLower(str1);
+    toLower(str2);
+
     if(anagrama(str1, str2)){
-        printf("SIM");
+        printf("SIM\n");
     } else {
-        printf("NAO");
+        printf("NAO\n");
     }
+
+    return 0;
 }
